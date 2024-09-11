b481e2dd1c5f7f517f789769af2f46266eac1333
Yaider <yaideralejo2@gmail.com>
2024-09-11 12:47:34 -0500

    Terminado el WorkBy State

--- src/uptc/views/appointments/byState/WorkByState.java
+++ src/uptc/views/appointments/byState/WorkByState.java
@@ -25,13 +25,11 @@ public class WorkByState extends JPanel {
         createTitle();
         createTable();
     }
-
     private void createTable() {
         JScrollPane scrollPane = new JScrollPane(tableByDateAppointments.getTable());
         scrollPane.setPreferredSize(obtainSizeForTable());
         add(scrollPane, BorderLayout.CENTER);
     }
-
     private void createTitle() {
         JPanel subHeaderPanel = new JPanel();
         subHeaderPanel.setBackground(Global.WORK_BACKGROUND_COLOR);
@@ -40,22 +38,19 @@ public class WorkByState extends JPanel {
         createTitle(subHeaderPanel);
         add(subHeaderPanel, BorderLayout.NORTH);
     }
-
     private void createTitle(JPanel panel) {
-        JLabel title = new JLabel("Citas por Fecha: ");
+        JLabel title = new JLabel("Registros ordenados por estado");
         title.setFont(Global.FONT_TITLE_BIG);
         title.setForeground(Global.WORK_TEXT_COLOR);
         title.setLayout(new FlowLayout(FlowLayout.LEFT));
         panel.add(title);
     }
-
     private Dimension obtainSizeForTable() {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         double width = screenSize.getWidth() * 0.95;
         double height = screenSize.getHeight() * 0.5;
         return new Dimension((int) width, (int) height);
     }
-
     private Dimension obtainSizeForSubHeader() {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         double width = screenSize.getWidth() * 0.95;
